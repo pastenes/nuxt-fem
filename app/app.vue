d968dc0292b90728383e682ee3dd01fd05eb26b6
Richard Pastenes <richard@pastenes.com>
2023-04-11 16:29:14 -0400

    01-exercise

---
+++ app/app.vue
@@ -0,0 +1,36 @@
+<script>
+import { defineNuxtComponent } from "#app";
+
+export default defineNuxtComponent({
+  data: () => ({
+    todoList: [],
+  }),
+  methods: {
+    fetchTodoList() {
+      fetch("https://jsonplaceholder.typicode.com/todos")
+        .then((response) => response.json())
+        .then((json) => {
+          this.todoList = json;
+        });
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <img src="todo.jpeg" alt="todo list" />
+    <h1>Sup</h1>
+    <button @click="fetchTodoList">Fetch!</button>
+    <!-- <pre>
+      {{ todoList }}
+    </pre> -->
+    <ul>
+      <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
+        <input type="checkbox" name="todo" :checked="todo.completed" />{{
+          todo.title
+        }}
+      </li>
+    </ul>
+  </div>
+</template>
