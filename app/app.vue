bf4a1ef60df4d7d83243a2a7281d51a3c0273621
Richard Pastenes <richard@pastenes.com>
2023-04-12 15:39:56 -0400

    Finished

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,38 @@
 <script></script>
 
+<script>
+import { defineNuxtComponent } from "#app";
+
+export default defineNuxtComponent({
+  data: () => ({
+    photoGallery: [],
+  }),
+  computed: {
+    numberOfPhotos() {
+      return this.photoGallery.length;
+    },
+  },
+  methods: {
+    fetchPhotoGallery() {
+      fetch("https://jsonplaceholder.typicode.com/photos")
+        .then((response) => response.json())
+        .then((json) => {
+          this.photoGallery = json;
+        });
+    },
+  },
+});
+</script>
+
 <template>
   <h1>Photo Gallery</h1>
+  <button @click="fetchPhotoGallery">Fetch!</button>
+  <p>{{ numberOfPhotos }} photos</p>
+  <ul>
+    <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+      <img :src="photo.thumbnailUrl" alt="photo.alt" />
+    </li>
+  </ul>
 </template>
 
 <style></style>
